2ba1285991b3301383853057eebefd03836610fd
zCranking <aravgupta2019@gmail.com>
2022-05-11 17:41:09 -0700

    THIS IS THE LAST TIME I WANT TO FIX A BUG (part 3)

--- imagethingy.py
+++ imagethingy.py
@@ -3,12 +3,12 @@ from PIL import ImageTk, Image
 from tkinter import filedialog
 
 root = Tk()
-root.geometry("700x700")
+root.geometry("1920x1080")
 root.title("Photoshop 2.0")
 root.config(background="#ff4e00")
 
 image = Label(root, image="", bg="#a3f7b5", highlightthickness=2, highlightcolor="#3590f3")
-image.place(relx=0.5, rely=0.1, anchor=CENTER)
+image.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 img_path = ""
 
@@ -16,7 +16,6 @@ def openimage():
     global image_path
     image_path = filedialog.askopenfilename(title = "Scanning your computers for viruses and then opening your image",
                                             filetypes = (("png", "*.png"),))
-    print(image_file)
     print(image_path)
     image_file = ImageTk.PhotoImage(Image.open(image_path))
     image['image'] = str(image_file)
