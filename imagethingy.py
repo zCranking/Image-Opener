af69375a93a89562e8581b17b862e868a13f8f9f
zCranking <aravgupta2019@gmail.com>
2022-05-20 17:27:16 -0700

    push

--- imagethingy.py
+++ imagethingy.py
@@ -19,9 +19,22 @@ def openimage():
     print(image_path)
     image_file = ImageTk.PhotoImage(Image.open(image_path))
     image['image'] = str(image_file)
-    image_path.close()
+    image_file.close()
     
-button = Button(root, text="Open Image",foreground="#ff4e00", font=("Comic Sans MS", "12", "normal"), command=openimage)
-button.place(relx=0.5, rely=0.8, anchor=CENTER)
+def rotateimage():
+    global image_path
+    image_file = ImageTk.PhotoImage(Image.open(image_path))
+    image['image'] = str(image_file)
+    openimage = open(img_path)
+    rotatedimage = openimage.rotate(90)
+    img = ImageTk.PhotoImage(Image.open(openimage))
+    image['image'] = str(img)
+    open_image.close()
+    
+rotatebutton = Button(root, text="Rotate Image",foreground="#ff4e00", font=("Comic Sans MS", "12", "normal"), command=openimage)
+rotatebutton.place(relx=0.57, rely=0.8, anchor=CENTER)   
+    
+openbutton = Button(root, text="Open Image",foreground="#ff4e00", font=("Comic Sans MS", "12", "normal"), command=openimage)
+openbutton.place(relx=0.5, rely=0.8, anchor=CENTER)
 
 root.mainloop()
